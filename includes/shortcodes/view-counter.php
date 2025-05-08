b1cd25ea24df77e870bf87ed817837814f211930
devfardin <contactfardin22@gmail.com>
2025-05-08 13:28:45 +0600

    create view counterfunction

---
+++ includes/shortcodes/view-counter.php
@@ -0,0 +1,21 @@
+<?php
+function axiusweb_view_counter(){
+    ob_start(); ?>
+
+    <?php
+    $countKey = 'post_views_count';
+    $count = get_post_meta(get_the_ID(), $countKey, true);
+    if($count==''){
+        $count = 0;
+        delete_post_meta(get_the_ID(), $countKey);
+        add_post_meta(get_the_ID(), $countKey, '0');
+    }else{
+        $count++;
+        update_post_meta(get_the_ID(), $countKey, $count);
+    };
+    ?>
+
+    <?php
+     return ob_get_clean();
+}
+add_shortcode( 'view_counter', 'axiusweb_view_counter' );
